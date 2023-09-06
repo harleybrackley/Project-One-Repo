10b205da0e55f377fe11d948d75a7ca2b933a2f1
Harley Brackley <harleybrackley@gmail.com>
2023-09-06 16:03:05 +1000

    some working logic

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const gameSquares = document.querySelectorAll(".game-square");
 const gameBoard = document.querySelector('.game-board');
 const resetButton = document.querySelector('.reset-button');
 const gameText = document.getElementById('game-text')
+
 const winningCombos = [
     [6, 7, 8],
     [3, 4, 5],
@@ -21,15 +22,15 @@ const winningCombos = [
 ]
 
 let currentPlayer = 'X';
-let picks = 0;
+let playerTurns = 0;
 
-const countO = 0;
-const countX = 0;
-const countTie = 0;
+const countO = document.querySelector('.tie-count');
+const countX = document.querySelector('.x-count')
+const countTie = document.querySelector('.o-count')
 
 let winCountO = 0;
 let winCountX = 0;
-let tieCountX = 0
+let tieCount = 0
 
 
 // Event listeners
@@ -41,16 +42,54 @@ resetButton.addEventListener('click', handleReset)
 
 // Functions - Event Handlers handle click, check win, check draw, reset game
 
-function handleGameSquares() {
-    
+// click box -> mark box -> check win/tie -> swap turns
+
+
+function handleGameSquares(evt) {
+    let chosenSquare = evt.target;
+    if (chosenSquare.innerHTML === ""){
+        chosenSquare.innerHTML = currentPlayer;
+        playerTurns++;
+        checkWin(currentPlayer);
+        if(playerTurns === 9) {
+            gameText.innerHTML = 'Tie Game!'
+            tieCount++
+            countTie.innerHTML = tieCount
+        }
+        if (currentPlayer === 'X'){
+            currentPlayer = 'O'
+        } else {
+            currentPlayer = 'X'
+        }
+        
+    } 
 }
+
+// check against winning combos array 
+
+function checkWin(player) {
+    for (let i = 0; i < winningCombos.length; i++) {
+        let winGame = winningCombos[i];
+        if(gameSquares[winGame[0]].innerHTML === player && gameSquares[winGame[1]].innerHTML === player && gameSquares[winGame[2]].innerHTML === player) {
+            gameText.innerHTML = `Congrats! Player ${player} wins!!`
+            if (player === 'X'){
+                winCountX++
+                countX.innerHTML = winCountX
+            } else if (player === 'O') {
+                winCountO++
+                countO.innerHTML = winCountO
+            }
+        }
+    }
+}
+
 function handleReset() {
+    gameSquares.forEach(function(gameSquare){
+        gameSquare.disabled = false
+        gameSquare.innerHTML = ""
 
-}
-function checkDraw() {
-
-}
-
-function checkWin() {
-    
+    })
+    currentPlayer = 'X';
+    turns = 0;
+    gameText.innerHTML = "";
 }
